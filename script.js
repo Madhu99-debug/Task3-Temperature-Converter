110c6ee84a3e406cbc8eb3f57a908ec3d71003b9
Madhu99-debug <119100594+Madhu99-debug@users.noreply.github.com>
2023-06-01 23:31:45 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+function convertTo(type) {
+    if (type === 'celsius') {
+        const celsiusInput = document.getElementById('celsius');
+        const fahrenheitInput = document.getElementById('fahrenheit');
+
+        if (celsiusInput.value) {
+            const celsius = parseFloat(celsiusInput.value);
+            const fahrenheit = (celsius * 9 / 5) + 32;
+            fahrenheitInput.value = fahrenheit.toFixed(2);
+        } else {
+            fahrenheitInput.value = '';
+        }
+    } else if (type === 'fahrenheit') {
+        const celsiusInput = document.getElementById('celsius');
+        const fahrenheitInput = document.getElementById('fahrenheit');
+
+        if (fahrenheitInput.value) {
+            const fahrenheit = parseFloat(fahrenheitInput.value);
+            const celsius = (fahrenheit - 32) * 5 / 9;
+            celsiusInput.value = celsius.toFixed(2);
+        } else {
+            celsiusInput.value = '';
+        }
+    }
+}
